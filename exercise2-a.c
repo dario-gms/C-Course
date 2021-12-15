ad3bac1f30d9f5a61ecccf3dd9bee98de156e4f4
Dário <86432208+dario-gms@users.noreply.github.com>
2021-12-15 11:28:44 -0300

    Exercise 2 part a

---
+++ exercise2-a.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+  // Analise os algoritmos abaixo e diga o que será impresso na tela ao serem executados:
+
+  // a) A <- 10
+  //    B <- 20
+  //   Escrever B
+  //   Escrever A, B
+
+  int a = 10;
+  int b = 20;
+
+  printf("valor de b é %d\n\n", b);
+
+  b = 5;
+
+  printf("valor de a %d e valor de b %d\n", a, b);
+
+  // Na primeira impressão de b o valor será 5. Na impressão de A e B os valores serão 10 e 5.
+}
