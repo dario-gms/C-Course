98a78de6dffcbb179061e0b7c88cbe19cf8943cd
Dário <86432208+dario-gms@users.noreply.github.com>
2022-01-03 15:23:09 -0300

    atualização da questão.

--- exercise2-a.c
+++ exercise2-a.c
@@ -5,15 +5,16 @@ int main () {
 
   // a) A <- 10
   //    B <- 20
-  //   Escrever B
-  //   Escrever A, B
+  //    Escrever B
+  //    B <- 5
+  //    Escrever A, B
 
   int a = 10;
   int b = 20;
 
   printf("valor de b é %d\n\n", b);
-
+  b = 5;
   printf("valor de a %d e valor de b %d\n", a, b);
 
-  // Na primeira impressão de b o valor será 5. Na impressão de A e B os valores serão 10 e 20.
+  // Na primeira impressão de b o valor será 5. Na impressão de A e B os valores serão 10 e 5.
 }
