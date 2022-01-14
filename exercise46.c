ebf31b04f810490522251cc9170d43641f1ff029
Dário <86432208+dario-gms@users.noreply.github.com>
2022-01-14 11:42:45 -0300

    exercise 46

---
+++ exercise46.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+  //Escreva um algoritmo para ler 2 valores e se o segundo valor informado for ZERO, deve ser lido um novo valor, ou seja, para o segundo valor não pode ser aceito o valor zero e imprimir o resultado da divisão do primeiro valor lido pelo segundo valor lido. (utilizar a estrutura REPITA).
+  //Acrescentar uma mensagem de 'VALOR INVÁLIDO' no exercício caso o segundo valor informado seja ZERO.
+
+  double num1 = 0;
+  double num2 = 0;
+  double resultado = 0;
+
+  printf("Informe o valor do primeiro número: \n");
+  scanf("%lf", &num1);
+  printf("Informe outro número: \n");
+  scanf("%lf", &num2);
+
+  if (num2 == 0) {
+    do {
+      printf("Número inválido!\n");
+      printf("Informe outro número:\n");
+      scanf("%lf", &num2);
+
+      resultado = num1 / num2;
+
+    } while(num2 == 0);
+    printf("O resultado da divisão do número %.2f e %.2f é %.2f.\n", num1, num2, resultado);
+  } else {
+    resultado = num1 / num2;
+    printf("O resultado da divisão do número %.2f e %.2f é %.2f.\n", num1, num2, resultado);
+  }
+
+  //Resultado
+  //Programa funcionou corretamente repetindo a mensagem cada vez que o usuário digita 0.
+
+}
