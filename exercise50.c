8ddacc58bbdb0744aa33a4037cbc51b94218606b
Dário <86432208+dario-gms@users.noreply.github.com>
2022-01-17 11:05:12 -0300

    exercise 50

---
+++ exercise50.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  //Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente.
+  for(int i = 1; i <= 10; i++)
+  printf("contagem %d / 10.\n", i);
+  
+
+  //Resultado
+  //Programa funcionou normalmente imprimindo de 1 a 10.
+}
