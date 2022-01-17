faf6b1ca6abcabe3feda9118ec9945da0185b85c
Dário <86432208+dario-gms@users.noreply.github.com>
2022-01-17 11:31:45 -0300

    exercise 51

---
+++ exercise51.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  // Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem decrescente.
+
+  for(int i = 10; i >= 1; i--){
+    printf("Ordem decrescente 10 de %d.\n", i);
+  }
+
+  //Resultado
+  //Programa funcionou corretamente imprimindo os números de 1 a 10 em ordem decrescente.  
+}
