28655c36f8c1b8dfbd520708c577083af2a50e02
Dário <86432208+dario-gms@users.noreply.github.com>
2022-01-17 12:57:25 -0300

    exercise 52

---
+++ exercise52.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  //Escreva um algoritmo para imprimir os 10 primeiros números inteiros maiores que 100.
+  
+  printf("Os 10 primeiros números inteiros maiores que 100 são:\n");
+  for (int i = 101; i <= 110; i++) {
+    printf("%d.\n", i);
+  }
+  //Resultado
+  //Programa funcionou corretamente imprimindo de 101 a 110.
+}
