f509d3af561a77d4527c26d197aa4f20b8a453f1
Dário <86432208+dario-gms@users.noreply.github.com>
2022-01-18 13:25:04 -0300

    exercise 57

---
+++ exercise57.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  //Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente utilizando a estrutura REPITA e um CONTADOR. 
+
+  int contador = 0;
+  do {
+    contador++;
+    printf("%d\n", contador);
+  } while (contador <= 9);
+
+  //Resultado
+  //Programa funcionou normalmente imprimindo os números de 1 a 10 em ordem crescente utilizando a estrutura repita e um contador. 
+}
