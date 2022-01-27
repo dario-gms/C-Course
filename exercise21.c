d92a09be8892a1ce6e2292326c9be41a4a6c517a
Dário <86432208+dario-gms@users.noreply.github.com>
2022-01-27 08:43:16 -0300

    update exercise 21

--- exercise21.c
+++ exercise21.c
@@ -6,14 +6,21 @@ int main() {
   int horastotais = 0;
   int horainicio = 0;
   int horafinal = 0;
+  int dia = 24;
 
   printf("Informe a hora que iniciou o jogo(Informar somente a hora, sem minutos e número de 0 a 24):\n");
   scanf("%d", &horainicio);
   printf("Informe a hora que terminou o jogo:\n");
   scanf("%d", &horafinal);
   
-  horastotais = horafinal - horainicio;
-  printf("A duração do jogo foi de %d horas.\n", horastotais);
+  
+  if (horainicio > horafinal) {
+    horastotais = (dia - horainicio) + horafinal;
+    printf("A duração do jogo foi de %d horas.\n", horastotais); 
+  } else {
+    horastotais = horafinal - horainicio;
+    printf("A duração do jogo foi de %d horas.\n", horastotais); 
+  }
 
   //Resultado
   //Programa funcionou corretamente exibindo a quantidade total de horas do jogo.
