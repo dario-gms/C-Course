e0b41ff97698b3b6f5c66d0512bac7bf6f8cc4a3
Dário <86432208+dario-gms@users.noreply.github.com>
2022-01-27 14:26:47 -0300

    Acrescentado a primeira função
    Primeira função inclusa no projeto chamada de abertura.

--- projeto2.c
+++ projeto2.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+void abertura() {
+  printf("*******************************\n");
+  printf("*       Jogo da forca         *\n");
+  printf("*******************************\n\n");
+}
+
 int main () {
   char palavrasecreta[20];
   
@@ -12,6 +18,8 @@ int main () {
   char chutes[26];
   int tentativas = 0;
 
+  abertura();
+  
   do {
     for (int i = 0; i < strlen(palavrasecreta); i++) {
       int achou = 0;
